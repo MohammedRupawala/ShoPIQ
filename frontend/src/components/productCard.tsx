102ae44c49fc4d4bc1f6d33949cbf654e6515080
Rupawala Mohammed Mustafa <mohammedrupawala8@gmail.com>
2024-06-28 10:10:07 +0530

    home page created

---
+++ frontend/src/components/productCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FaPlus } from "react-icons/fa"
+type ProductProps = {
+    photo:string
+    productId : string
+    stock : number
+    price : number
+    name :string
+    handler : ()=>void
+}
+
+const ProductCard = ({name,photo,productId,stock,price,handler}:ProductProps) => {
+  return (
+    <div className='productCard'>
+         <img src={photo} alt={name}/>
+         <p>name</p>
+         <span>${price}</span>
+        <div>
+            <button onClick={()=>handler()}>
+                <FaPlus/>
+            </button>
+        </div>
+    </div>
+    
+  )
+}
+
+export default ProductCard
